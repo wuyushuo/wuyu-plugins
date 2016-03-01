c56de3025e472b3c01f15204ce5154f597563943
pudongping <dongping.pu@yinyuetai.com>
2016-03-01 15:47:21 +0800

    wuyu-plugins

---
+++ wuyu-plugin-utils/src/main/java/com/wuyu/plugin/utils/ZhUtil.java
@@ -0,0 +1,77 @@
+/*--------------------------------------------------------------------------
+ *  Copyright (c) 2009-2020, dennisit.pu All rights reserved. 
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ * Neither the name of the yinyuetai developer nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ * Author: dennisit.pu (dennisit@163.com)
+ *--------------------------------------------------------------------------
+*/
+package com.wuyu.plugin.utils;
+
+/**
+ * class function depict
+ * created on 2016/3/1
+ * @author dennisit@163.com
+ * @version 1.0
+ */
+public class ZhUtil {
+
+    public static final boolean isHasChinese(String strName) {
+        char[] ch = strName.toCharArray();
+        for (int i = 0; i < ch.length; i++) {
+            char c = ch[i];
+            if (isChinese(c)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static final boolean isChinese(char c) {
+        Character.UnicodeBlock ub = Character.UnicodeBlock.of(c);
+        if (ub == Character.UnicodeBlock.CJK_UNIFIED_IDEOGRAPHS
+                || ub == Character.UnicodeBlock.CJK_COMPATIBILITY_IDEOGRAPHS
+                || ub == Character.UnicodeBlock.CJK_UNIFIED_IDEOGRAPHS_EXTENSION_A
+                || ub == Character.UnicodeBlock.GENERAL_PUNCTUATION
+                || ub == Character.UnicodeBlock.CJK_SYMBOLS_AND_PUNCTUATION
+                || ub == Character.UnicodeBlock.HALFWIDTH_AND_FULLWIDTH_FORMS) {
+            return true;
+        }
+        return false;
+    }
+
+
+    public static final boolean isContainChinese(String chineseStr) {
+        char[] charArray = chineseStr.toCharArray();
+        for (int i = 0; i < charArray.length; i++) {
+            if ((charArray[i] >= 0x4e00) && (charArray[i] <= 0x9fbb)) {
+                //Java判断一个字符串是否有中文是利用Unicode编码来判断，
+                // 因为中文的编码区间为：0x4e00--0x9fbb
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args){
+        System.out.println(isHasChinese("ceshi"));
+        System.out.println(isHasChinese("ceshi中文"));
+        System.out.println(isHasChinese("中文文本串"));
+
+        System.out.println("================");
+
+        System.out.println(isContainChinese("ceshi"));
+        System.out.println(isContainChinese("ceshi中文"));
+        System.out.println(isContainChinese("中文文本串"));
+    }
+}
