52cbc69c218f47f00767a7b1bc99ac7d10a5e6d0
pudongping <dongping.pu@yinyuetai.com>
2016-03-01 15:57:16 +0800

    wuyu-plugins

---
+++ wuyu-plugin-mvc/src/main/java/com/wuyu/plugin/mvc/advice/JsonpResponseAdvice.java
@@ -0,0 +1,46 @@
+/*--------------------------------------------------------------------------
+ *  Copyright (c) 2009-2020, www.wuyushuo.com All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ * Neither the name of the wuyushuo developer nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ * Author: git_wuyu@163.com (tencent qq: 2094998377)
+ *--------------------------------------------------------------------------
+*/
+package com.wuyu.plugin.mvc.advice;
+
+import org.springframework.http.MediaType;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.AbstractJsonpResponseBodyAdvice;
+
+/**
+ * organization <a href="http://www.wuyushuo.com">www.wuyushuo.com</a>
+ * created on 2016/02/04 by <strong>elon su</strong>
+ * email addr (<a href='mailto:git_wuyu@163.com'>git_wuyu@163.com</a>)
+ * @version 1.0.0
+ */
+@Controller
+@ControllerAdvice
+public class JsonpResponseAdvice extends AbstractJsonpResponseBodyAdvice {
+
+    public JsonpResponseAdvice() {
+        super("callback");
+    }
+
+    @Override
+    protected MediaType getContentType(MediaType contentType, ServerHttpRequest request, ServerHttpResponse response) {
+        return new MediaType("application", "javascript", contentType.getCharSet());
+    }
+}
--- wuyu-plugin-mvc/src/main/java/com/wuyu/plugin/mvc/filter/PerformanceFilter.java
+++ wuyu-plugin-mvc/src/main/java/com/wuyu/plugin/mvc/filter/PerformanceFilter.java
@@ -1,21 +1,21 @@
-//--------------------------------------------------------------------------
-// Copyright (c) 2010-2020, En.dennisit or Cn.苏若年
-// All rights reserved.
-//
-// Redistribution and use in source and binary forms, with or without
-// modification, are permitted provided that the following conditions are
-// met:
-//
-// Redistributions of source code must retain the above copyright notice,
-// this list of conditions and the following disclaimer.
-// Redistributions in binary form must reproduce the above copyright
-// notice, this list of conditions and the following disclaimer in the
-// documentation and/or other materials provided with the distribution.
-// Neither the name of the dennisit nor the names of its contributors
-// may be used to endorse or promote products derived from this software
-// without specific prior written permission.
-// Author: dennisit@163.com | dongping.pu | 苏若年
-//--------------------------------------------------------------------------
+/*--------------------------------------------------------------------------
+ *  Copyright (c) 2009-2020, www.wuyushuo.com All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ * Neither the name of the wuyushuo developer nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ * Author: git_wuyu@163.com (tencent qq: 2094998377)
+ *--------------------------------------------------------------------------
+*/
 package com.wuyu.plugin.mvc.filter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -29,7 +29,7 @@ import java.io.IOException;
 /**
  * organization <a href="http://www.wuyushuo.com">www.wuyushuo.com</a>
  * created on 2016/02/04 by <strong>elon su</strong>
- * email addr (<a href='mailto:git_wuyu@163.com'></>git_wuyu@163.com</a>)
+ * email addr (<a href='mailto:git_wuyu@163.com'>git_wuyu@163.com</a>)
  * @version 1.0.0
  */
 public class PerformanceFilter implements Filter {
